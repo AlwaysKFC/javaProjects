31c1323bdf78252b8938eb3a07d343f42e77f810
maacevedom@udistrital.edu.co <142860380+AlwaysKFC@users.noreply.github.com>
2023-08-22 17:49:40 -0500

    Add files via upload

---
+++ Mis primeros codigos/SumaResta.java
@@ -0,0 +1,20 @@
+package sumaresta;
+
+public class SumaResta {
+    
+    public static void main(String[] args) {
+        
+        int Zebras = 8;
+        int Tigres = 6;
+        
+        int Animales = Zebras + Tigres;
+        
+        System.out.println("hay " + Animales + " animales en el Zoo");
+        
+        int ZebrasDespuesDelCambio = Zebras -4;
+        
+        System.out.println("Hay " + ZebrasDespuesDelCambio + " Zebras despues del cambio");
+        
+    }
+    
+}
