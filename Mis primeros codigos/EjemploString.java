fb53c3b388b1c11865b3716680e7f81f24befaa8
maacevedom@udistrital.edu.co <142860380+AlwaysKFC@users.noreply.github.com>
2023-08-22 17:38:35 -0500

    Add files via upload

---
+++ Mis primeros codigos/EjemploString.java
@@ -0,0 +1,13 @@
+package ejemplostring;
+
+public class EjemploString {
+
+    public static void main(String[] args) {
+
+        String Cancion = "Si tu me hubieras dicho siempre la verdad, si huebieras respondido cuando te llame...";
+        
+        System.out.println(Cancion);
+        
+    }
+    
+}
