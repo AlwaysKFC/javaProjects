e495f0d5caf6b46b004e139da77c4e20d547acd1
maacevedom@udistrital.edu.co <142860380+AlwaysKFC@users.noreply.github.com>
2023-08-22 17:45:25 -0500

    Add files via upload

---
+++ Mis primeros codigos/Arbol.java
@@ -0,0 +1,22 @@
+package arbol;
+
+public class Arbol {
+
+    public static void main(String[] args) {
+        
+       System.out.println("Hey there, I'm Martin and i'm learning to code in java!");
+       System.out.println("I'm going to plant a tree Today");
+       System.out.println("   *   ");
+       System.out.println("  ***  ");
+       System.out.println("*******");
+       System.out.println("  ***  ");
+       System.out.println("   *   ");
+       System.out.println("   *   ");
+       System.out.println("   *   ");
+       System.out.println("   *   ");
+       System.out.println("   *   ");
+       System.out.println("_______");
+        
+    }
+    
+}
