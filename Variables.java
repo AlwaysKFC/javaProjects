4563b458c5f385f284ee90466146144dc817031d
AlwaysKFC <142860380+AlwaysKFC@users.noreply.github.com>
2023-08-22 17:06:21 -0500

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class Variables {
+
+    public static void main(String[] args) {
+            String name = "Martin Acevedo";
+            
+            int Age = 17;
+            
+            System.out.println(name);
+            
+            System.out.println(Age);
+    }
+    
+}
