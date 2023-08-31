b734bd86b3e4944d37a2e9ae051bd2ad29b9967f
maacevedom@udistrital.edu.co <142860380+AlwaysKFC@users.noreply.github.com>
2023-08-31 16:49:59 -0500

    Add files via upload

---
+++ Condicionales/ConditionalExamples.java
@@ -0,0 +1,10 @@
+public class ConditionalExamples{
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 32;
+        
+        if(x+y > 25){
+            System.out.println("x + y is greater than 25")
+        }
+    }
+}
