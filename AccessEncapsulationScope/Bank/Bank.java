cdae037570c32d49f7a487f0a620c02e9a2a224e
maacevedom@udistrital.edu.co <142860380+AlwaysKFC@users.noreply.github.com>
2023-09-19 17:25:12 -0500

    Add files via upload

---
+++ AccessEncapsulationScope/Bank/Bank.java
@@ -0,0 +1,9 @@
+public class CheckingAccount{
+    String name;
+    int balance;
+
+    public CheckingAccount(String inputName, int inputBalance){
+        name = inputName;
+        balance = inputBalance;
+    }
+}
