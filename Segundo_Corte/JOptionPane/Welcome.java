4be5a956ace97abd789f23c83c4d738c529e5df6
maacevedom@udistrital.edu.co <142860380+AlwaysKFC@users.noreply.github.com>
2023-10-03 16:59:37 -0500

    Add files via upload

---
+++ Segundo_Corte/JOptionPane/Welcome.java
@@ -0,0 +1,12 @@
+package welcome;
+
+import javax.swing.JOptionPane;
+
+public class Welcome {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Welcome\nto\nthe Java World");
+        System.exit(0);
+    }
+    
+}
