614e4ba34be6e17e9476480a6a3f465d7ee0191a
maacevedom@udistrital.edu.co <142860380+AlwaysKFC@users.noreply.github.com>
2023-11-20 18:55:38 -0500

    Add files via upload

---
+++ Segundo_Corte/movie/main.java
@@ -0,0 +1,10 @@
+package movie;
+
+import javax.swing.JOptionPane;
+import movie.ui.UIPeliculas;
+
+public class main {
+    public static void main(String[] args){
+        UIPeliculas.interfazUsuario();
+    }
+}
